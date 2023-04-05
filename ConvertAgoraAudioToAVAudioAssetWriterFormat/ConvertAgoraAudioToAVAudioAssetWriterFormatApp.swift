50d66557639591615b6889fe04f17051df8cbcd7
Shaun Hubbard <shaunhubbard2013@icloud.com>
2023-04-05 16:17:51 -0400

    Initial Commit

---
+++ ConvertAgoraAudioToAVAudioAssetWriterFormat/ConvertAgoraAudioToAVAudioAssetWriterFormatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertAgoraAudioToAVAudioAssetWriterFormatApp.swift
+//  ConvertAgoraAudioToAVAudioAssetWriterFormat
+//
+//  Created by Shaun Hubbard on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ConvertAgoraAudioToAVAudioAssetWriterFormatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
